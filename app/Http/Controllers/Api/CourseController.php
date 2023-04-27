eb160522d9eef54d1a26779704000e5adb79f5d9
Mithuya <yokarasa.mithuya@gmail.com>
2023-04-28 03:49:49 +0530

    REST Service Providing Component Done

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CourseResource;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $courses = Course::get();
+        return (["timeline" => CourseResource::collection($courses)]);
+    }
+}
